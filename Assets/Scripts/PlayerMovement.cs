4262260500518c7dc15fa297354cbd714eb53adb
af1799 <atf52@cornell.edu>
2025-06-28 13:58:53 +0800

    Revert "Merge branch 'battery-and-health' into object_interaction"
    This reverts commit f5a4b966a2283101fd0b9331d4927a18f73278c0, reversing
    changes made to c85bf72336dd59872914ee799110b7502e8025f1.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -18,7 +18,6 @@ public class PlayerMovement : MonoBehaviour
     }
 
     [Header("Movement")]
-    public bool hasBatteryForJumpAndSprint;
     public float sprintSpeed;
     public float walkSpeed;
     public float wallRunSpeed;
@@ -70,8 +69,6 @@ public class PlayerMovement : MonoBehaviour
 
         playerHeight = GetComponentInChildren<CapsuleCollider>().height * gameObject.transform.localScale.y;
 
-        hasBatteryForJumpAndSprint = true;
-
         // if (!volume.profile.TryGet(out vignette)) {
         //     Debug.LogWarning("No Vignette component found on Global Volume");
         // }
@@ -111,8 +108,7 @@ public class PlayerMovement : MonoBehaviour
         if (Grounded)
         {
             if (coyoteTimeCounter > 0f && jumpBufferCounter > 0f
-            && movementState != MovementState.CROUCH
-            && hasBatteryForJumpAndSprint)
+            && movementState != MovementState.CROUCH)
             {
                 Jump();
 
@@ -198,7 +194,7 @@ public class PlayerMovement : MonoBehaviour
                 moveSpeed = crouchSpeed / Time.timeScale;
             }
 
-            else if (InputController.Instance.GetSprint() && hasBatteryForJumpAndSprint) {
+            else if (InputController.Instance.GetSprint()) {
                 movementState = MovementState.SPRINT;
                 moveSpeed = sprintSpeed / Time.timeScale;
             }

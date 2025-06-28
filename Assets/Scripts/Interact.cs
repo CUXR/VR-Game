237231cc0cadde0f135a1031d909f2fd36c32257
af1799 <atf52@cornell.edu>
2025-06-28 14:52:08 +0800

    fixed some bugs with picking up object

--- Assets/Scripts/Interact.cs
+++ Assets/Scripts/Interact.cs
@@ -1,21 +1,57 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Interact : MonoBehaviour, Interactable
 {
-    // When this function is called, the object's position is transformed to a point relative to the player's
-    // position.
+    private Boolean holding = false;
+
+    private float width;
+
+    private float x;
+    private float y;
+    private float z;
+
+    GameObject player;
+
+
+    // Object's collider is disabled, and the holding variable is 
+    // updated.
     public void Grab(GameObject gObject)
     {
-        Vector3 myPosition = gObject.transform.position;
+        player = gObject;
+        Collider collider = GetComponent<Collider>();
+        MeshRenderer renderer = GetComponent<MeshRenderer>();
+        Vector3 size = renderer.bounds.size;
+        width = size.x;
 
-        float x = myPosition.x;
-        float y = myPosition.y;
-        float z = myPosition.z;
+        collider.enabled = false;
 
-        transform.position = new Vector3(x, y, z);
+        holding = true;
 
+    }
+
+    // Collider is enabled and holding is updated to false.
+    public void Release()
+    {
+        Collider collider = GetComponent<Collider>();
+        collider.enabled = true;
+        holding = false;
+    }
+
+    // If holding is true, then object's position is updated to a new position relative to the player's view.
+    void Update()
+    {
+        if (holding == true)
+        {
+            Vector3 myPosition = player.transform.GetChild(0).GetChild(0).position;
+
+            x = myPosition.x;
+            y = myPosition.y;
+            z = myPosition.z;
+            transform.position = new Vector3(x, y, z + width);
+        }
     }
 }
 
--- Assets/Scripts/InteractInterface.cs
+++ Assets/Scripts/InteractInterface.cs
@@ -3,12 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-public interface Interactable {
+public interface Interactable
+{
 
     // This function is called by the player being in range of an interactable object and pressing the
-    // "e" key. As long as the player holds the key, that is the amount of time that the object is 
-    // being interacted with. The effects of the interation depend on the characteristics of the object.
-    // Takes in a GameObject (usually the player) as an argument.
-        public void Grab(GameObject gObject);
+    // "e" key (applied only if the player is not currently holding an object). The effects of the
+    // interaction depend on the characteristics of the object. Object generally remains in view of
+    // the player. Takes in a GameObject (usually the player) as an argument.
+    public void Grab(GameObject gObject);
+
+    // This function is called when the player is holding an object and presses the "e" key. The
+    // object is effectively released, and it is typically removed from the player's influence.
+    public void Release();
 }
 

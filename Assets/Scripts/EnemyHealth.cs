ac088d3f8f28851b5f9ec893872fa88fca73c9de
af1799 <atf52@cornell.edu>
2025-08-19 14:23:38 -0500

    enemy health

--- Assets/Scripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyHealth.cs
@@ -1,17 +1,14 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Runtime.CompilerServices;
-using Unity.VisualScripting;
-using UnityEditor.PackageManager;
 using UnityEngine;
 
 public class EnemyHealth : MonoBehaviour
 {
-    public Boolean alive = true;
+    public bool alive = true;
     public void stab()
     {
         alive = false;
         Debug.Log("Enemy killed");
     }
 }
+

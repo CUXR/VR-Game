fe1df614ae49829a439be247b2a0384a25523f3c
af1799 <atf52@cornell.edu>
2025-08-19 14:44:02 -0500

    fixed

--- Assets/Scripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyHealth.cs
@@ -4,11 +4,36 @@ using UnityEngine;
 
 public class EnemyHealth : MonoBehaviour
 {
+    public GameObject player;
     public bool alive = true;
+    private Combat combat;
+    private Outline outline;
+
+    void Start()
+    {
+        combat = player.GetComponent<Combat>();
+        outline = gameObject.AddComponent<Outline>();
+        outline.enabled = false;
+        outline.OutlineColor = Color.red;
+        outline.OutlineWidth = 12.0f;
+    }
+
     public void stab()
     {
         alive = false;
         Debug.Log("Enemy killed");
+    }
+
+    void Update()
+    {
+        if (combat.enemyInRange && combat.enemyObject == gameObject)
+        {
+            outline.enabled = true;
+        }
+        else
+        {
+            outline.enabled = false;
+        }
     }
 }
 

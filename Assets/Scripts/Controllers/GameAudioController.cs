05347333d6d2e75c825d1c4151aa8149f1ab1ac2
af1799 <atf52@cornell.edu>
2025-09-17 17:26:56 -0400

    environment layer

--- Assets/Scripts/Controllers/GameAudioController.cs
+++ Assets/Scripts/Controllers/GameAudioController.cs
@@ -6,8 +6,6 @@ public class AudioController : MonoBehaviour
 {
     public static AudioController Instance;
     private List<EnemyController> enemies;
-    private float soundVolume;
-    private float muffling;
     private void Awake()
     {
         if (Instance != null && Instance != this)
@@ -25,40 +23,30 @@ public class AudioController : MonoBehaviour
     {
         if (enemies != null)
         {
-          foreach (EnemyController enemy in enemies)
+            foreach (EnemyController enemy in enemies)
             {
+                float muffling = 1f;
                 float distance = Vector3.Distance(enemy.transform.position, sound.position);
                 if (Overlap(enemy.hearing.GetRange(), sound.radius, distance))
                 {
                     Vector3 direction = (enemy.transform.position - sound.position).normalized;
-                    if (Physics.Raycast(sound.position, direction, sound.radius))
+                    if (Physics.Raycast(sound.position, direction, distance))
                     {
                         muffling = 0.5f;
                     }
-                    else
-                    {
-                        muffling = 1f;
-                    }
-                    soundVolume = sound.loudness * muffling * Mathf.Exp(-sound.decayRate * distance);
+                    float soundVolume = sound.loudness * muffling * Mathf.Exp(-sound.decayRate * distance);
                     if (soundVolume >= enemy.hearing.GetThreshold())
                     {
                         enemy.hearing.HeardSound(sound.position);
                     }
                 }
-            }  
+            }
         }
     }
 
     private bool Overlap(float firstRadius, float secondRadius, float distance)
     {
-        if (distance <= (firstRadius + secondRadius))
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
+         return distance <= (firstRadius + secondRadius);
     }
 
     public void EnemyChanged()

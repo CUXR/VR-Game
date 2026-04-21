f5d9c0bd5ea46fa30136cdc0186847ac9dd48c25
Annabelle <atf52@cornell.edu>
2026-04-21 15:17:45 -0400

    fixed glow/tutorial?

--- Assets/Scripts/Player/PlayerObjectInteraction.cs
+++ Assets/Scripts/Player/PlayerObjectInteraction.cs
@@ -16,15 +16,15 @@ public class ObjectInteraction : MonoBehaviour
 
     void Update()
     {
-        // The ray is determined by the camera position, the hit variable is where the
-        // information about what the ray hits will be stored, and the range variable represents
-        // the numerical range in which an object can be interacted with
-        if (Physics.Raycast(Camera.main.gameObject.transform.position,
-            Camera.main.gameObject.transform.forward, out RaycastHit hit, range))
-            // If an object is within the range, is hit by the raycast
+        if (held==null)
         {
-            if (hit.collider.gameObject.TryGetComponent(out InteractableInterface interactableObject))
-                // If the object hit has an Interactable component
+            // The ray is determined by the camera position, the hit variable is where the
+            // information about what the ray hits will be stored, and the range variable represents
+            // the numerical range in which an object can be interacted with
+            if (Physics.Raycast(Camera.main.gameObject.transform.position,
+                Camera.main.gameObject.transform.forward, out RaycastHit hit, range)
+                && hit.collider.gameObject.TryGetComponent(out InteractableInterface interactableObject))
+                // If an object is within the range, is hit by the raycast
             {
                 if (lastHighlighted != interactableObject)
                 {
@@ -38,33 +38,38 @@ public class ObjectInteraction : MonoBehaviour
                     if (interactableObject is Movable movableObject)
                     {
                         movableObject.SetInteractor(gameObject.transform, pushForce);
-                    }
-                    if (interactableObject is Holdable holdableObject)
+                    } else if (interactableObject is Holdable holdableObject)
                     {
-                        if (held==null)
-                        {
-                            held = holdableObject;
-                        } else
-                        {
-                            held.Release();
-                            held = null;
-                            return;
-                        }
+                        held = holdableObject;
                     }
                     interactableObject.Interact();
                 }
             } else
             {
-                TutorialController.Instance.ClearText();
-                lastHighlighted?.SetGlow(false);
-                lastHighlighted=null;
+                ClearGlow();
             }
-
+        } else {
+            ClearGlow();
+            if (InputController.Instance.GetInteractDown())
+            {
+                held.Release();
+                held = null;
+            }
         }
         if (Input.GetKeyDown(KeyCode.Mouse0) && held != null && held is Holdable interact)
         {
             interact.Throw();
             held = null;
+        }
+    }
+
+    private void ClearGlow()
+    {
+        if (lastHighlighted!=null)
+        {
+            TutorialController.Instance.ClearText();
+            lastHighlighted?.SetGlow(false);
+            lastHighlighted=null; 
         }
     }
 }
